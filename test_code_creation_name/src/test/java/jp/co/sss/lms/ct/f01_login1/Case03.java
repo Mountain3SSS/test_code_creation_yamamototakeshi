2f59ef3100682ff28ee2d70204ef82fc7ea088ff
user <user@192.168.0.28>
2025-11-27 14:32:53 +0900

    fix: Case03.javaテストケース03：ログイン(正常系)のテストコードを追加。
    初回ログイン済ユーザー情報を記載しログインすることで、以下を確認する。
    なお確認方法はログイン試行後のページタイトルがコース詳細画面のものか否かで判断する
    
    確認点
    1. ログインに成功すること
    2. ログイン成功時コース詳細画面へ正しく遷移すること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +39,16 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//トップページへ遷移
+		String topPage = "http://localhost:8080/lms/";
+		goTo(topPage);
+
+		//エビデンス取得
+		getEvidence(new Case01() {
+		});
+
+		//現ページがログインページである点確認
+		assertEquals(webDriver.getCurrentUrl(), topPage);
 	}
 
 	@Test
@@ -43,6 +56,29 @@ public class Case03 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
-	}
+		//ログイン情報投入先を指定
+		WebElement user = webDriver.findElement(By.id("loginId"));
+		WebElement password = webDriver.findElement(By.id("password"));
 
+		//上記DBに登録されていないユーザー情報を投入
+		user.sendKeys("StudentAA01");
+		password.sendKeys("StudentAA011");
+
+		// 上記情報が入力されているログイン画面でエビデンスを取得
+		getEvidence(new Case03() {
+		}, "1_ログイン情報");
+
+		//ログイン処理と表示
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn-primary"));
+		loginButtonElement.click();
+
+		// 画面表示まで待機、その後ログイン成功の確認
+		pageLoadTimeout(5);
+
+		assertEquals(webDriver.getTitle(), "コース詳細 | LMS");
+
+		// エラーメッセージが表示されているログイン画面でエビデンスを取得
+		getEvidence(new Case03() {
+		}, "2_ログイン成功");
+	}
 }
