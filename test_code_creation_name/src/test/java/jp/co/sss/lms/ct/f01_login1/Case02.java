7eda5447684d9f6422937fb8068c6e2dd6b11f16
user <user@192.168.0.28>
2025-11-27 11:31:33 +0900

    add:Case02.java ログイン画面への遷移、DBに登録されていないユーザーでログインを行った際のテストコード追加 test02にてWebElementを利用してDBに登録されていないユーザー情報を記入し、ログインを試行した際期待通りの結果となるか確認するテストを作成した。期待内容はログイン画面へ遷移し、エラーメッセ維持が表示されることとする。 なお、ログイン画面への遷移はCase01と同じものを利用している

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +39,16 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//トップページへ遷移
+		String topPage = "http://localhost:8080/lms/";
+		goTo(topPage);
+
+		//エビデンス取得
+		getEvidence(new Case01() {
+		}, "Case01-01");
+
+		//現ページがログインページである点確認
+		assertEquals(webDriver.getCurrentUrl(), topPage);
 	}
 
 	@Test
@@ -43,6 +56,30 @@ public class Case02 {
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
-	}
 
+		//ログイン情報投入先を指定
+		WebElement user = webDriver.findElement(By.id("loginId"));
+		WebElement password = webDriver.findElement(By.id("password"));
+
+		//上記DBに登録されていないユーザー情報を投入
+		user.sendKeys("test");
+		password.sendKeys("test");
+
+		// 上記情報が入力されているログイン画面でエビデンスを取得
+		getEvidence(new Case02() {
+		}, "1_ログインするのに利用した情報");
+
+		//ログイン処理
+		WebElement loginButtonElement = webDriver.findElement(By.className("btn-primary"));
+		loginButtonElement.click();
+
+		// ログイン失敗の確認
+		WebElement errorMessageElement = webDriver.findElement(By.className("help-inline"));
+		assertEquals(errorMessageElement.getText(), "* ログインに失敗しました。");
+
+		// エラーメッセージが表示されているログイン画面でエビデンスを取得
+		getEvidence(new Case02() {
+		}, "2_ログイン失敗");
+
+	}
 }
